862a63e4ee0228a3cfb56b4b9480e48679b3576e
Paulo Pereira <paulovspereira@gmail.com>
2021-06-15 16:23:20 -0300

    Começando a calculadora

--- src/main/java/br/com/erudio/GreetingController.java
+++ src/main/java/br/com/erudio/GreetingController.java
@@ -2,8 +2,9 @@ package br.com.erudio;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +13,11 @@ public class GreetingController {
 	private static final String template = "Hello, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
-	@RequestMapping("/greeting")
-	public Greeting greeting(@RequestParam(value = "name", defaultValue = "word") String name) {
-		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	@RequestMapping(value="/sum/numberOne/numberTwo ", method=RequestMethod.GET)
+	public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+		return  1D;
 
 	}
 }
+
+//PathVariabel = Passar um variavel no caminho 
